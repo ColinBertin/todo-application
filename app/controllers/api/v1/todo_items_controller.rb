323b203ce45f0cfdf1f40f9785e83677e3516c10
Colin Bertin <colinbertin12@gmail.com>
2022-04-27 15:56:12 +0900

    authentification + root for the api

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_todo_item, only: %i[show edit update destroy]
+
+  def index
+    @todo_items = TodoItem.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_todo_item
+    @todo_item = TodoItem.find(params[:id])
+  end
+
+  def authorized?
+    @todo_item.user == current_user
+  end
 end
